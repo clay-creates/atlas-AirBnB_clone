caef009ee8c9d2bd6c75dad63a096adeed349cb5
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-02 18:35:22 +0000

    implement args and kwargs in __init__

--- models/base_model.py
+++ models/base_model.py
@@ -7,13 +7,20 @@ import uuid
 
 class BaseModel:
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialization for BaseModel
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, "%Y-%m-%d%T%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))

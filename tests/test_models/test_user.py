6bd00049ff6fae92fbf6660e3bbcbaef85086353
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-03 20:47:01 +0000

    make test_user (no tests made)

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module containing unittests for User class
+"""
+import unittest
+from models.base_model import BaseModel
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+
+    

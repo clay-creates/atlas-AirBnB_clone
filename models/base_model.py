98d21cbddbc90c3589201edbe114d719de2f0a71
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-02-03 00:09:04 +0000

    fixed pycodestyle errors on base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ This module defines the BaseModel class, parent for all other classes
 from datetime import datetime
 import uuid
 
+
 class BaseModel:
 
     def __init__(self, *args, **kwargs):
@@ -15,7 +16,8 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        value = datetime.strptime(value,
+                                                  "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
@@ -23,7 +25,8 @@ class BaseModel:
             self.updated_at = self.created_at
 
     def __str__(self):
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
 
     def save(self):
         """

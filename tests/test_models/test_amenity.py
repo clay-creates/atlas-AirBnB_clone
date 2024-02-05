aa4516ee0179823eff184b8d3b08199c078ba6e4
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-05 01:47:11 +0000

    make amenity test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -9,4 +9,11 @@ from models.amenity import Amenity
 
 class TestAmenity(unittest.TestCase):
 
-    
+    def setUp(self):
+        self.amenity_model = Amenity()
+
+    def test_initialization(self):
+        self.assertIsInstance(self.amenity_model.name, str)
+
+if __name__ == "__main__":
+    unittest.main()

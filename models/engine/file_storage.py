55208d6d895dcc7f66544372c53d1758d6928d3b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-05 23:29:01 +0000

    change to file_storage to resolve typeerror

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,7 +28,7 @@ class FileStorage:
         """
         Sets a new object in __objects with key
         """
-        key = f"{obj['__class__']}.{obj['id']}"
+        key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
